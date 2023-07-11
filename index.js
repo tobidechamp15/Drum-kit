b9121f212addfa652dfdce8b7c85d038cd201d97
Olubukola Oluwadare <Olubukola.Oluwadare@ubagroup.com>
2023-07-11 22:04:13 +0100

    ass-done

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// Detecting Keyboard press
-
 document.addEventListener("keydown", handleKeyDown);
 
-function handleKeyDown() {}
-
-// Sound Function
+function handleKeyDown(event) {
+  makingSound(event.key);
+  buttonAnimation(event.key);
+}
 
 function makingSound(key) {
   switch (key) {
@@ -12,21 +11,46 @@ function makingSound(key) {
       var crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
+    case "a":
+      var crash = new Audio("sounds/snare.mp3");
+      crash.play();
+      break;
+    case "s":
+      var crash = new Audio("sounds/kiss-bass.mp3");
+      crash.play();
+      break;
+    case "d":
+      var crash = new Audio("sounds/tom-4.mp3");
+      crash.play();
+      break;
+    case "j":
+      var crash = new Audio("sounds/tom-1.mp3");
+      crash.play();
+      break;
+    case "k":
+      var crash = new Audio("sounds/tom-2.mp3");
+      crash.play();
+      break;
+    case "l":
+      var crash = new Audio("sounds/tom-3.mp3");
+      crash.play();
+      break;
 
     default:
-      console.log(keyValue);
+      console.log(key);
       break;
   }
 }
 
-// Button Animation function
-
 function buttonAnimation(currentKey) {
   var activeButton = document.querySelector("." + currentKey);
 
-  activeButton.classList.add("pressed");
+  if (activeButton) {
+    // Checking if the button exists
+    activeButton.classList.add("pressed");
 
-  setTimeout(function () {
-    activeButton.classList.remove("pressed");
-  }, 100);
+    setTimeout(function () {
+      activeButton.classList.remove("pressed");
+    }, 100);
+  }
 }
